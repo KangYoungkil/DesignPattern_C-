c716d5ab005ec6aa17c54cc4c9f080ac0141a840
Bross <Bross@Brossui-MacBook-Air.local>
2014-09-25 17:08:06 +0900

    Initial Commit

---
+++ DesignPattern_Task_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DesignPattern_Task_1
+//
+//  Created by Bross on 2014. 9. 25..
+//  Copyright (c) 2014년 Bross. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
